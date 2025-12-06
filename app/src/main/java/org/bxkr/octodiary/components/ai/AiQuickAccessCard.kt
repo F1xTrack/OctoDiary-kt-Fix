20c99c1c7cf83906240965331a521e6c2c1114b2
F1xTrack <vladik130310@gmail.com>
2025-12-07 02:03:55 +0300

    Fix TextOverflow import error

--- app/src/main/java/org/bxkr/octodiary/components/ai/AiQuickAccessCard.kt
+++ app/src/main/java/org/bxkr/octodiary/components/ai/AiQuickAccessCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextOverflow
 import org.bxkr.octodiary.Screen
 import org.bxkr.octodiary.navControllerLive
 
